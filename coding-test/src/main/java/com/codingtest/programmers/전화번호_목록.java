54da41ed1ae757cfea5fac3d25418febdad32a01
dlwnsgus777 <dlwnsgus777@naver.com>
2025-11-07 12:26:09 +0900

    feat: 문제풀이

---
+++ coding-test/src/main/java/com/codingtest/programmers/전화번호_목록.java
@@ -0,0 +1,17 @@
+package com.codingtest.programmers;
+
+import java.util.Arrays;
+
+public class 전화번호_목록 {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ coding-test/src/test/java/com/codingtest/programmers/전화번호_목록Test.java
@@ -0,0 +1,32 @@
+package com.codingtest.programmers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class 전화번호_목록Test {
+
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void test01(String[] participant, boolean answer) {
+        전화번호_목록 solution = new 전화번호_목록();
+        boolean sut = solution.solution(participant);
+        assertThat(sut).isEqualTo(answer);
+    }
+
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+            Arguments.of(
+                new String[] {"119", "97674223", "1195524421"},
+               false
+                        ),
+            Arguments.of(
+                new String[] {"123", "456", "78945612312312312312"},
+                false
+                        )
+                        );
+    }
+}
