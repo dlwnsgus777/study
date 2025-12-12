e5b5adc7f0f7ea91b422464a92b944367d9943ed
dlwnsgus777 <dlwnsgus777@naver.com>
2025-12-12 09:17:06 +0900

    feat: dfs 구현

---
+++ coding-test/src/main/java/com/codingtest/beakjoon/DFS와BFS.java
@@ -0,0 +1,54 @@
+package com.codingtest.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DFS와BFS {
+
+    public static StringBuilder SB = new StringBuilder();
+    public static int[][] arr;
+    public static boolean[] visit;
+    public static int n;
+    public static int m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] st = bf.readLine()
+                        .split(" ");
+
+        n = Integer.parseInt(st[0]);
+        m = Integer.parseInt(st[1]);
+        int start = Integer.parseInt(st[2]);
+
+        arr = new int[n + 1][n + 1];
+        visit = new boolean[n + 1];
+
+        for (int i = 1; i < m + 1; i++) {
+            String[] st2 = bf.readLine()
+                            .split(" ");
+            int a = Integer.parseInt(st2[0]);
+            int b = Integer.parseInt(st2[1]);
+
+            arr[a][b] = arr[b][a] = 1;
+        }
+
+        dfs(start);
+        SB.append("\n");
+
+        System.out.println(SB.toString());
+
+        bf.close();
+    }
+
+    public static void dfs(int start) {
+        visit[start] = true;
+        SB.append(start + " ");
+
+        for (int i = 1; i < n + 1; i++) {
+            if (arr[start][i] == 1 && !visit[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
